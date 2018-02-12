f8c16ab69ca20ff476574934c3e92f0bf1b4123e
pintokha17 <pintokha.s@gmail.com>
2018-02-12 20:42:07 +0100

    logout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,6 +84,14 @@
                     @auth
                         <a href="{{ url('/tasks') }}">Tasks</a>
                         <a href="{{ url('/reports') }}">Reports</a>
+                        <a href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
